f4c1fc1018039ebcb71085d04f50877c9e13b546
DenCreave <dencreave@gmail.com>
2023-09-05 18:27:35 +0200

    basic dIgeTs

--- SirenDisplay/Classes/MyBottomLeftPathFigure.cs
+++ SirenDisplay/Classes/MyBottomLeftPathFigure.cs
@@ -1,8 +1,21 @@
+using Avalonia;
+using Avalonia.Media;
 using SirenDisplay.Interfaces;
 
 namespace SirenDisplay.Classes;
 
 public class MyBottomLeftPathFigure : IMyPathfigure
 {
+    public Point StartPoint => new Point(10, 50);
+
+    public LineSegment[] LineSegments => new[]
+    {
+        new LineSegment { Point = new Point(20, 60) },
+        new LineSegment { Point = new Point(20, 80) },
+        new LineSegment { Point = new Point(10, 90) },
+        new LineSegment { Point = new Point(0, 80) },
+        new LineSegment { Point = new Point(0, 60) },
+        new LineSegment { Point = new Point(10, 50) },
+    };
     
 }
