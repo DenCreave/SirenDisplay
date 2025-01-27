c09340ce179964fb3f097d3ae1d8c930541139e3
Vaento <dencreave@gmail.com>
2025-01-27 13:21:01 +0100

    clock now shows time

--- SirenDisplay/Assets/Polygons/Digits/DigitLoader.cs
+++ SirenDisplay/Assets/Polygons/Digits/DigitLoader.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Avalonia.Media;
 using SirenDisplay.Interfaces;
 
 namespace SirenDisplay.Classes.Digits;
@@ -9,7 +10,13 @@ namespace SirenDisplay.Classes.Digits;
 public sealed class DigitLoader
 {
     public Dictionary<int, IMyDigit> Digits { get; }
-
+    public PathGeometry[] PathGeometries { get; }
+    
+    public PathGeometry ReturnPathGeometry( int index )
+    {
+        return PathGeometries[index];
+    }
+    
     public DigitLoader()
     {
         Digits = new();
@@ -40,5 +47,20 @@ public sealed class DigitLoader
             Console.WriteLine(e);
             throw;
         }
+        
+        
+        PathGeometries = new PathGeometry[Digits.Count+1]; //PathGeometries[10] is an empty pathgeometry
+        for (int i = 0; i < 10; i++)
+        {
+            PathGeometries[i] = new PathGeometry
+            {
+                Figures = Digits[i].PathFigures
+            };
+        }
+
+        PathGeometries[10] = new PathGeometry()
+        {
+            Figures = new PathFigures()
+        };
     }
 }
--- SirenDisplay/ViewModels/ClockViewModel.cs
+++ SirenDisplay/ViewModels/ClockViewModel.cs
@@ -1,12 +1,16 @@
 using System;
+using System.Runtime.InteropServices.JavaScript;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Shapes;
 using Avalonia.Layout;
 using Avalonia.Media;
+using Avalonia.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
-
+using ExCSS;
 using SirenDisplay.Assets.Polygons.Frames;
+using SirenDisplay.Classes.Digits;
+using Color = Avalonia.Media.Color;
 
 namespace SirenDisplay.ViewModels;
 
@@ -14,16 +18,19 @@ public sealed partial class ClockViewModel : ObservableObject
 {
     
     [ObservableProperty] private Path _mypathFigures;
+    [ObservableProperty] private Path _hourDecimalDigit;
+    [ObservableProperty] private Path _minuteDecimalDigit;
+    [ObservableProperty] private Path _hourDigit;
+    [ObservableProperty] private Path _minuteDigit;
     public ClockViewModel()
     {
         
-        //var backgroundGrid = this.FindControl<Grid>("BackGroundGrid"); 
         MypathFigures = new Path
         {
 
             Stroke =  new SolidColorBrush(Color.FromArgb(255, 0xff, 0x90, 0x1b)),
             StrokeThickness = 10,
-            Margin = new Thickness(5),
+            //Margin = new Thickness(5),
             Stretch = Stretch.Fill,
             Data = new PathGeometry
             {
@@ -41,7 +48,111 @@ public sealed partial class ClockViewModel : ObservableObject
                 BlurRadius = 500
             },
         };
-        Console.WriteLine("ClockViewModel");
+        Console.WriteLine("ClockViewModel and something else");
+        UpdateTime();
+    }
+    
+    private void UpdateTime()
+    {
+        var framerate = TimeSpan.FromSeconds(1);
+        var timer = new DispatcherTimer
+        {
+            Interval = framerate
+        };
+        int i = 0;
+        
+        DigitLoader digitLoader = new DigitLoader();
+        
+        int hours = DateTime.Now.Hour;
+        int hoursDecimal = hours / 10;
+        int minutes = DateTime.Now.Minute;
+        int minutesDecimal = minutes / 10;
+        HourDigit= new Path
+        {
+
+            Stroke =  new SolidColorBrush(Color.FromArgb(255, 0xff, 0x90, 0x1b)),
+            StrokeThickness = 10,
+            //Margin = new Thickness(5),
+            Stretch = Stretch.Fill,
+            Data = digitLoader.ReturnPathGeometry(10),
+            Effect = new DropShadowEffect
+            {
+                Color = Color.FromArgb(255, 0xff, 0x90, 0x1b),
+                Opacity = 1,
+                BlurRadius = 500
+            }
+        };
+        
+        HourDecimalDigit= new Path
+        {
+
+            Stroke =  new SolidColorBrush(Color.FromArgb(255, 0xff, 0x90, 0x1b)),
+            StrokeThickness = 10,
+            //Margin = new Thickness(5),
+            Stretch = Stretch.Fill,
+            Data = digitLoader.ReturnPathGeometry(10),
+            Effect = new DropShadowEffect
+            {
+                Color = Color.FromArgb(255, 0xff, 0x90, 0x1b),
+                Opacity = 10,
+                BlurRadius = 500
+            }
+        };
+        
+        MinuteDecimalDigit = new Path
+        {
+
+            Stroke =  new SolidColorBrush(Color.FromArgb(255, 0xff, 0x90, 0x1b)),
+            StrokeThickness = 10,
+            //Margin = new Thickness(5),
+            Stretch = Stretch.Fill,
+            Data = digitLoader.ReturnPathGeometry(10),
+            Effect = new DropShadowEffect
+            {
+                Color = Color.FromArgb(255, 0xff, 0x90, 0x1b),
+                Opacity = 1,
+                BlurRadius = 500
+            }
+        };
+        
+        MinuteDigit = new Path
+        {
+
+            Stroke =  new SolidColorBrush(Color.FromArgb(255, 0xff, 0x90, 0x1b)),
+            StrokeThickness = 10,
+            //Margin = new Thickness(5),
+            Stretch = Stretch.UniformToFill,
+            Data = digitLoader.ReturnPathGeometry(10),
+            Effect = new DropShadowEffect
+            {
+                Color = Color.FromArgb(255, 0xff, 0x90, 0x1b),
+                Opacity = 1,
+                BlurRadius = 500
+            }
+        };
+        
+        timer.Tick += (sender, args) =>
+        {
+            int hours = DateTime.Now.Hour;
+            int hoursDecimal = hours / 10;
+            int minutes = DateTime.Now.Minute;
+            int minutesDecimal = minutes / 10;
+            if (hoursDecimal == 0)
+            {
+                HourDecimalDigit.Data=digitLoader.ReturnPathGeometry(10); 
+            }
+            else
+            { 
+                HourDecimalDigit.Data=digitLoader.ReturnPathGeometry(hoursDecimal%3);
+            }
+            HourDigit.Data=digitLoader.ReturnPathGeometry(hours%10);
+            MinuteDecimalDigit.Data=digitLoader.ReturnPathGeometry(minutesDecimal%6);
+            MinuteDigit.Data=digitLoader.ReturnPathGeometry(minutes%10);
+            i++;
+            Console.WriteLine(i);
+
+        };
+        timer.Start();
         
     }
 }
