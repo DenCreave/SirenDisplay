c263c6ce27d6135a3461a090f61464f58d7e8401
Vaento <dencreave@gmail.com>
2025-02-17 00:15:11 +0100

    Mc Orsen - Warning (God mode)

---
+++ SirenDisplay/ViewModels/MusicViewModel.cs
@@ -0,0 +1,6 @@
+namespace SirenDisplay.ViewModels;
+
+public sealed partial class MusicViewModel : ViewModelBase
+{
+    
+}
---
+++ SirenDisplay/Views/MusicView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SirenDisplay.Views;
+
+public partial class MusicView : UserControl
+{
+    public MusicView()
+    {
+        InitializeComponent();
+    }
+}
