e14305f6394060118b1c2e623764fd323da8c6b4
Vaento <dencreave@gmail.com>
2025-05-25 18:29:44 +0200

    Keiichi Okabe - Weight of the World oopsie

--- SirenDisplay/Views/InitView.axaml.cs
+++ SirenDisplay/Views/InitView.axaml.cs
@@ -12,7 +12,7 @@ public partial class InitView : Window
     {
         InitializeComponent();
         DataContext = new InitViewModel();
-       // WindowState = WindowState.FullScreen;
-       // Cursor = new Cursor(StandardCursorType.None); //comment this line if you want to see the cursor
+        WindowState = WindowState.FullScreen;
+        Cursor = new Cursor(StandardCursorType.None); //comment this line if you want to see the cursor
     }
 }
