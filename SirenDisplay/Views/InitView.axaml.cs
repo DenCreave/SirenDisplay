6c750b867330472eb1e34c8755a80d9a22467c78
Vaento <dencreave@gmail.com>
2026-07-02 17:15:11 +0200

    sghenny - charlie chaplin - frenchcore

--- SirenDisplay/Views/InitView.axaml.cs
+++ SirenDisplay/Views/InitView.axaml.cs
@@ -13,7 +13,7 @@ public partial class InitView : Window
     {
         InitializeComponent();
         DataContext = App.Services.GetRequiredService<InitViewModel>();
-        //WindowState = WindowState.FullScreen;
-        //Cursor = new Cursor(StandardCursorType.None);  //comment this line if you want to see the cursor
+        WindowState = WindowState.FullScreen;
+        Cursor = new Cursor(StandardCursorType.None);  //comment this line if you want to see the cursor
     }
 }
